1d35eb0201e0d1069a770cc1a692402218a8d17b
Justin Linville <justinlinville@awesomenesstv.com>
2018-07-11 15:00:53 -0700

    add in new commands to back and restore a database

--- postgres/docker-entrypoint.d/init-db.sh
+++ postgres/docker-entrypoint.d/init-db.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER atv_user;
-    CREATE DATABASE video;
-    CREATE DATABASE video_development;
-    CREATE DATABASE video_test;
-    GRANT ALL PRIVILEGES ON DATABASE video TO atv_user;
-    GRANT ALL PRIVILEGES ON DATABASE video_development TO atv_user;
-    GRANT ALL PRIVILEGES ON DATABASE video_test TO atv_user;
-EOSQL
+: ${DB_NAME:=$1}
+: ${DB_DUMP_PATH:=/db/dumps/latest.dump}
+
+dropdb -U postgres -e --if-exists $1 && \
+createdb -U postgres $1 && \
+pg_restore -U postgres --dbname=$1 \
+ --clean --no-owner --no-privileges --verbose --jobs=3 \
+ $DB_DUMP_PATH
