940465033a95435feb36068ffff3d1698a2c2b7d
Stephen Neptune <steven.neptune@gmail.com>
2018-07-16 13:55:30 -0700

    moved around init for postgresdb.

--- postgres/post-install.d/load-dump.sh
+++ postgres/post-install.d/load-dump.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-: ${DB_NAME:=$1}
+: ${DB_NAME:='video_manager-development'}
 : ${DB_USER:='postgres'}
 : ${DB_DUMP_PATH:='/db/dumps/latest.dump'}
 
 if [ -e $DB_DUMP_PATH ]
 then
-  dropdb -U $DB_USER -e --if-exists $1 && \
-  createdb -U $DB_USER $1 && \
-  pg_restore -U $DB_USER --dbname=$1 \
+  dropdb -U $DB_USER -e --if-exists $DB_NAME && \
+  createdb -U $DB_USER $DB_NAME && \
+  pg_restore -U $DB_USER --dbname=$DB_NAME \
    --clean --no-owner --no-privileges --verbose --jobs=3 \
-  $DB_DUMP_PATH && \
+  $DB_DUMP_PATH  && \
   echo "Merged in latest database dump.";
 fi
